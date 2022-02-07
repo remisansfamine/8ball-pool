c2ed8aee384b559d260adec4ae3a72d3c8959792
r.giner <remi.giner@hotmail.fr>
2022-02-07 12:03:50 +0100

    [dev] Adding gravity
    Co-authored-by: Benjamin MARIN <marinbenjaminpro@gmail.com>
    Co-authored-by: Leo LORCET <lorcetleo@gmail.com>

--- Assets/Scripts/Physics/PhysicsParameter.cs
+++ Assets/Scripts/Physics/PhysicsParameter.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class PhysicsParameter : MonoBehaviour
 {
+    [SerializeField] private bool isStatic;
+
     [SerializeField] protected float momentOfInertia = 0.1f;
 
-    [SerializeField] protected Vector3 velocity;
+    [SerializeField] public Vector3 velocity;
     public Vector3 Velocity => velocity;
 
     [SerializeField] protected Vector3 angularVelocity;
     [SerializeField] protected float frictionCoef = 1f;
     [SerializeField] protected float angularFrictionCoef = 1f;
-    [SerializeField] protected float jkrtse = ;
 
-    [SerializeField] protected float mass = 1f;
+    [SerializeField, Range(0.00001f, Mathf.Infinity)] protected float mass = 1f;
     public float Mass => mass;
 
     [SerializeField] protected float radius = 0.5f;
+
+    public void AddForce(Vector3 force)
+    {
+        if (!isStatic)
+            velocity += Time.fixedDeltaTime / mass * force;
+        //AddLocalTorque(-velocity, Vector3.down);
+    }
+
+    public void AddTorque(Vector3 torque)
+    { 
+        if (!isStatic)
+            angularVelocity += Time.fixedDeltaTime / momentOfInertia * torque;
+        //AddForce(Quaternion.Euler(angularVelocity * Time.fixedDeltaTime) * velocity * jkrtse);
+        //velocity = Quaternion.Euler(angularVelocity * Time.fixedDeltaTime) * velocity;
+        //Debug.Log("Velocity = " + velocity);
+    }
+    public void AddTorque(Vector3 force, Vector3 position) => AddTorque(Vector3.Cross(position - transform.position, force));
+    public void AddLocalTorque(Vector3 force, Vector3 position) => AddTorque(Vector3.Cross(position, force));
 }
