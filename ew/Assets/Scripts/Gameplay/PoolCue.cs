1b6ccb9f9a7baae50428e37bd473e85f808b52c6
Benjamin Marin <b.marin@student.isartdigital.com>
2022-02-08 22:48:56 +0100

    Cue 3D movements added

---
+++ ew/Assets/Scripts/Gameplay/PoolCue.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolCue : MonoBehaviour
+{
+    [SerializeField] private Vector2 sensivity;
+    [SerializeField] private RangeAttribute rangeRotationX = new RangeAttribute(-75f, 0f);
+
+    private Vector2 rotation;
+
+    void RotateCue()
+    {
+        Vector2 axis;
+        axis.x = Input.GetAxis("Mouse X");
+        axis.y = Input.GetAxis("Mouse Y");
+
+        rotation.y = rotation.y - axis.x * sensivity.y * Time.deltaTime;
+        rotation.x = Mathf.Clamp(rotation.x - axis.y * sensivity.x * Time.deltaTime, rangeRotationX.min, rangeRotationX.max);
+        transform.eulerAngles = new Vector3(rotation.x, rotation.y, transform.eulerAngles.z);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            RotateCue();
+    }
+}
